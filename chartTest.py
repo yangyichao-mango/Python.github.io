645b798b66d162298d2bb09de729ca1c2075f972
yyc123xn <1048262223@qq.com>
2018-01-04 19:41:59 +0800

    avg

--- chartTest.py
+++ chartTest.py
@@ -1,6 +1,10 @@
-
-
 import pymysql
+
+import plotly
+plotly.tools.set_credentials_file(username='yyc', api_key='1puNznRHHWs5kPei8ESP')
+
+import plotly.plotly as py
+from plotly.graph_objs import *
 
 con = pymysql.connect(
     host="localhost",
@@ -13,34 +17,100 @@ con = pymysql.connect(
 )
 cur = con.cursor()
 
-sql = "SELECT type,AVG(price) FROM Airbnb_Shanghai GROUP BY type"
-
-cur.execute(sql)
-result = cur.fetchall()
-print(result)
-
-roomType=[]
-price=[]
-
-for field in result:
-    roomType.append(field[0])
-    price.append(int(field[1]))
-
-print(roomType)
-print(price)
+city = ''
+trace0 = ''
+trace1 = ''
+trace2 = ''
 
 
-import plotly
-plotly.tools.set_credentials_file(username='yyc', api_key='yfwQuYOyxJAeamQvdivC')
+for i in range(0,3,1):
+    if i == 2:
+        city = 'Shanghai'
+    if i == 1:
+        city = 'Beijing'
+    if i == 0:
+        city = 'Chengdu'
 
-import plotly.plotly as py
-from plotly.graph_objs import *
+    sql = "SELECT type,AVG(price) FROM Airbnb_"+city+" GROUP BY type"
 
-trace0 = Scatter(
-    x = roomType,
-    y = price
-)
+    cur.execute(sql)
+    result = cur.fetchall()
+    print(result)
 
-data = Data([trace0])
+    roomType=[]
+    price=[]
 
-py.plot(data, filename = 'basic-line')
+    for field in result:
+        roomType.append(field[0])
+        price.append(int(field[1]))
+
+    if i == 2:
+        Shanghai = Scatter(
+            x=roomType,
+            y=price,
+        )
+    if i == 1:
+        Beijing = Scatter(
+            x=roomType,
+            y=price,
+        )
+    if i == 0:
+        Chengdu = Scatter(
+            x=roomType,
+            y=price,
+        )
+
+
+    print(roomType)
+    print(price)
+
+data = Data([Shanghai])
+
+py.plot(data, filename = 'basic-line1')
+
+data = Data([Beijing])
+
+py.plot(data, filename = 'basic-line2')
+
+data = Data([Chengdu])
+
+py.plot(data, filename = 'basic-line3')
+
+
+for i in range(0,3,1):
+    if i == 2:
+        city = 'Shanghai'
+    if i == 1:
+        city = 'Beijing'
+    if i == 0:
+        city = 'Chengdu'
+
+    sql = "SELECT AVG(price) FROM Airbnb_"+city+" "
+
+    cur.execute(sql)
+    result = cur.fetchall()
+    print(result)
+
+    price=int(result[0][0])
+
+    print(price)
+
+    if i == 2:
+        Shanghai = Scatter(
+            x="Shanghai",
+            y=price,
+        )
+    if i == 1:
+        Beijing = Scatter(
+            x="Beijing",
+            y=price,
+        )
+    if i == 0:
+        Chengdu = Scatter(
+            x="Chengdu",
+            y=price,
+        )
+
+data = Data([Chengdu,Shanghai,Beijing])
+
+py.plot(data, filename = 'basic-line4')
--- python.py
+++ python.py
@@ -19,92 +19,184 @@ cursor = con.cursor()
 
 driver = webdriver.Chrome()
 
+city = ''
+cityUrl = ''
 
-count = 0
-for i in range(0, 17 , 1):
+#上海 https://zh.airbnb.com/s/Shanghai--China?cdn_cn=1&s_tag=ejxIlhR7&allow_override%5B%5D=&section_offset={}
+#北京 https://zh.airbnb.com/s/Beijing--China?cdn_cn=1&s_tag=kN1Hdq18&allow_override%5B%5D=&section_offset={}
+#成都 https://zh.airbnb.com/s/Chengdu-Shi?cdn_cn=1&s_tag=DV8jh0yp&allow_override%5B%5D=&section_offset={}
+for j in range(0,3,1):
+    if j == 2:
+        city = 'Shanghai'
+        cityUrl = 'Shanghai--China?cdn_cn=1&s_tag=ejxIlhR7'
+    if j == 1:
+        city = 'Beijing'
+        cityUrl = 'Beijing--China?cdn_cn=1&s_tag=kN1Hdq18'
+    if j == 0:
+        city = 'Chengdu'
+        cityUrl = 'Chengdu-Shi?cdn_cn=1&s_tag=DV8jh0yp'
 
-    url = 'https://zh.airbnb.com/s/Shanghai--China/homes?cdn_cn=1&s_tag=ejxIlhR7&allow_override%5B%5D=&section_offset={}'.format(
-        str(i))
+    #爬取数据
 
-    if i == 0:
-        url = 'https://zh.airbnb.com/s/Shanghai--China/homes?cdn_cn=1&s_tag=ejxIlhR7&allow_override%5B%5D='
+    for i in range(0, 17 , 1):
 
-    driver.get(url)
-    # print(i)
-    #if i == 1270:
-    #   elements = driver.find_elements_by_class_name('mod-1')
-    #  for element in elements:
-    #     element.click()
+        url = ('https://zh.airbnb.com/s/'+cityUrl+'&allow_override%5B%5D=&section_offset={}').format(
+            str(i))
 
-    time.sleep(2)
-    html = driver.page_source#获取网页的html数据、
-    soup = bs4.BeautifulSoup(html,'lxml')#对html进行解析
-    r = soup.find_all('div', class_='_v72lrv')
+        if i == 0:
+            url = 'https://zh.airbnb.com/s/'+cityUrl+'&allow_override%5B%5D='
 
-    for tag in r:
+        driver.get(url)
+        # print(i)
+        #if i == 1270:
+        #   elements = driver.find_elements_by_class_name('mod-1')
+        #  for element in elements:
+        #     element.click()
 
-        url = 'https://zh.airbnb.com/'+str(tag.find('a',class_="_j4ns53m").get('href'))
-        title = str(tag.find('div',class_="_ew0cqip").text)
-        priceContent = tag.find('span',class_="_hylizj6")
-        priceTmp = priceContent.find_all('span')[2].text
-        price = priceTmp[1:].replace(',', '')
+        time.sleep(2)
+        html = driver.page_source#获取网页的html数据、
+        soup = bs4.BeautifulSoup(html,'lxml')#对html进行解析
+        r = soup.find_all('div', class_='_v72lrv')
 
-        # price = str(priceContent.find('span').text)
-        type = str(tag.find('small',class_='_5y5o80m').text)
+        for tag in r:
 
-        if tag.find('span',class_='_gb7fydm'):
-            comments = tag.find('span',class_='_gb7fydm').text
-        else:
-            comments = 'NEW'
+            url = 'https://zh.airbnb.com/'+str(tag.find('a',class_="_j4ns53m").get('href'))
+            title = str(tag.find('div',class_="_ew0cqip").text)
+            priceContent = tag.find('span',class_="_hylizj6")
+            priceTmp = priceContent.find_all('span')[2].text
+            price = priceTmp[1:].replace(',', '')
 
-        print(url)
-        print(title)
-        print(price)
-        print(type)
-        print(comments + '\n')
+            # price = str(priceContent.find('span').text)
+            type = str(tag.find('small',class_='_5y5o80m').text)
 
-        sql = "INSERT INTO Airbnb_Shanghai(url,price,type,comments) values('" \
-              + url +"'," + price + ",'" + type + "','" + comments +"')"
-        print(sql)
-        cursor.execute(sql)
-        con.commit()
+            if tag.find('span',class_='_gb7fydm'):
+                comments = tag.find('span',class_='_gb7fydm').text
+            else:
+                comments = 'NEW'
 
-        sql = "UPDATE Airbnb_Shanghai " \
-              "SET title = '"+title.replace("'", '').replace('"', '')+"' " \
-              "WHERE title = '0'"
-        print(sql)
-        cursor.execute(sql)
-        con.commit()
+            print(url)
+            print(title)
+            print(price)
+            print(type)
+            print(comments + '\n')
+
+            sql = "INSERT INTO Airbnb_"+city+"(url,price,type,comments) values('" \
+                  + url +"'," + price + ",'" + type + "','" + comments +"')"
+            print(sql)
+            cursor.execute(sql)
+            con.commit()
+
+            sql = "UPDATE Airbnb_"+city+" " \
+                  "SET title = '"+title.replace("'", '').replace('"', '')+"' " \
+                  "WHERE title = '0'"
+            print(sql)
+            cursor.execute(sql)
+            con.commit()
 
 # 图标展示
-sql = "SELECT type,AVG(price) FROM Airbnb_Shanghai GROUP BY type"
-
-cursor.execute(sql)
-result = cursor.fetchall()
-print(result)
-
-roomType=[]
-price=[]
-
-for field in result:
-    roomType.append(field[0])
-    price.append(int(field[1]))
-
-print(roomType)
-print(price)
-
 
 import plotly
-plotly.tools.set_credentials_file(username='yyc', api_key='yfwQuYOyxJAeamQvdivC')
+plotly.tools.set_credentials_file(username='yyc', api_key='1puNznRHHWs5kPei8ESP')
 
 import plotly.plotly as py
 from plotly.graph_objs import *
 
-trace0 = Scatter(
-    x = roomType,
-    y = price
-)
 
-data = Data([trace0])
 
-py.plot(data, filename = 'basic-line')
+city = ''
+trace0 = ''
+trace1 = ''
+trace2 = ''
+
+
+for i in range(0,3,1):
+    if i == 2:
+        city = 'Shanghai'
+    if i == 1:
+        city = 'Beijing'
+    if i == 0:
+        city = 'Chengdu'
+
+    sql = "SELECT type,AVG(price) FROM Airbnb_"+city+" GROUP BY type"
+
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    print(result)
+
+    roomType=[]
+    price=[]
+
+    for field in result:
+        roomType.append(field[0])
+        price.append(int(field[1]))
+
+    if i == 2:
+        Shanghai = Scatter(
+            x=roomType,
+            y=price,
+        )
+    if i == 1:
+        Beijing = Scatter(
+            x=roomType,
+            y=price,
+        )
+    if i == 0:
+        Chengdu = Scatter(
+            x=roomType,
+            y=price,
+        )
+
+
+    print(roomType)
+    print(price)
+
+data = Data([Shanghai])
+
+py.plot(data, filename = 'basic-line1')
+
+data = Data([Beijing])
+
+py.plot(data, filename = 'basic-line2')
+
+data = Data([Chengdu])
+
+py.plot(data, filename = 'basic-line3')
+
+
+for i in range(0,3,1):
+    if i == 2:
+        city = 'Shanghai'
+    if i == 1:
+        city = 'Beijing'
+    if i == 0:
+        city = 'Chengdu'
+
+    sql = "SELECT AVG(price) FROM Airbnb_"+city+" "
+
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    print(result)
+
+    price=int(result[0][0])
+
+    print(price)
+
+    if i == 2:
+        Shanghai = Scatter(
+            x="Shanghai",
+            y=price,
+        )
+    if i == 1:
+        Beijing = Scatter(
+            x="Beijing",
+            y=price,
+        )
+    if i == 0:
+        Chengdu = Scatter(
+            x="Chengdu",
+            y=price,
+        )
+
+data = Data([Chengdu,Shanghai,Beijing])
+
+py.plot(data, filename = 'basic-line4')
