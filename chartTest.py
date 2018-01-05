dbc61259af1042898d2d98f7cc4ea5f2e9401f06
yyc123xn <1048262223@qq.com>
2018-01-05 09:47:52 +0800

    chartAdd

--- chartTest.py
+++ chartTest.py
@@ -1,10 +1,16 @@
+# 图标展示
+
+
 import pymysql
+
 
 import plotly
 plotly.tools.set_credentials_file(username='yyc', api_key='1puNznRHHWs5kPei8ESP')
 
 import plotly.plotly as py
 from plotly.graph_objs import *
+
+
 
 con = pymysql.connect(
     host="localhost",
@@ -15,7 +21,8 @@ con = pymysql.connect(
     use_unicode=True,
     charset="utf8"
 )
-cur = con.cursor()
+cursor = con.cursor()
+
 
 city = ''
 trace0 = ''
@@ -33,8 +40,8 @@ for i in range(0,3,1):
 
     sql = "SELECT type,AVG(price) FROM Airbnb_"+city+" GROUP BY type"
 
-    cur.execute(sql)
-    result = cur.fetchall()
+    cursor.execute(sql)
+    result = cursor.fetchall()
     print(result)
 
     roomType=[]
@@ -90,8 +97,8 @@ for i in range(0,3,1):
 
     sql = "SELECT AVG(price) FROM Airbnb_"+city+" "
 
-    cur.execute(sql)
-    result = cur.fetchall()
+    cursor.execute(sql)
+    result = cursor.fetchall()
     print(result)
 
     price=int(result[0][0])
@@ -127,8 +134,8 @@ sql = "SELECT Airbnb_Shanghai.type,AVG(Airbnb_Shanghai.price),AVG(Airbnb_Chengdu
       "JOIN Airbnb_Beijing ON Airbnb_Shanghai.type = Airbnb_Beijing.type " \
       "GROUP BY type"
 
-cur.execute(sql)
-result = cur.fetchall()
+cursor.execute(sql)
+result = cursor.fetchall()
 print(result)
 roomType=[]
 shanghaiPrice=[]
@@ -168,3 +175,115 @@ print(BeijingPrice)
 data = Data([Chengdu,Shanghai,Beijing])
 
 py.plot(data, filename = 'compare')
+
+city = ''
+y1 = ''
+y2 = ''
+y3 = ''
+#分数比较
+for i in range(0,3,1):
+    if i == 2:
+        city = 'Shanghai'
+    if i == 1:
+        city = 'Beijing'
+    if i == 0:
+        city = 'Chengdu'
+
+    sql = " SELECT (SELECT COUNT(*) FROM airbnb_"+city+" AS "+city+" WHERE "+city+".score = 3) AS '3' ," \
+				"IFNULL((SELECT AVG(price) FROM airbnb_"+city+" AS "+city+" WHERE "+city+".score = 3),0) AS '3Price' ," \
+				"(SELECT COUNT(*) FROM airbnb_"+city+" AS "+city+" WHERE "+city+".score = 4) AS '4' ," \
+				"IFNULL((SELECT AVG(price) FROM airbnb_"+city+" AS "+city+" WHERE "+city+".score = 4),0) AS '4Price' ," \
+				"(SELECT COUNT(*) FROM airbnb_"+city+" AS "+city+" WHERE "+city+".score = 4.5) AS '4.5' ," \
+				"IFNULL((SELECT AVG(price) FROM airbnb_"+city+" AS "+city+" WHERE "+city+".score = 4.5),0) AS '4.5Price' ," \
+				"(SELECT COUNT(*) FROM airbnb_"+city+" AS "+city+" WHERE "+city+".score = 5) AS '5' ," \
+				"IFNULL((SELECT AVG(price) FROM airbnb_"+city+" AS "+city+" WHERE "+city+".score = 5),0) AS '5Price' " \
+            "FROM airbnb_"+city+" LIMIT 1 "
+
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    print(result)
+
+    price=[int(result[0][1]),int(result[0][3]),int(result[0][5]),int(result[0][7])]
+    num=[int(result[0][0]),int(result[0][2]),int(result[0][4]),int(result[0][6])]
+
+    print(price)
+
+    if i == 2:
+        Shanghai = Scatter(
+            x=['3','4','4.5','5'],
+            y=price,
+            name='Shanghai'
+        )
+        y1 = num
+    if i == 1:
+        Beijing = Scatter(
+            x=['3', '4', '4.5', '5'],
+            y=price,
+            name='Beijing'
+        )
+        y2 = num
+    if i == 0:
+        Chengdu = Scatter(
+            x=['3', '4', '4.5', '5'],
+            y=price,
+            name='Chengdu'
+        )
+        y3 = num
+
+
+data = Data([Chengdu,Shanghai,Beijing])
+
+py.plot(data, filename = 'score')
+
+#各个评分段的房屋个数
+dataset = {'x': ['3', '4', '4.5', '5'],
+           'y1': y1,
+           'y2': y2,
+           'y3': y3}
+
+# 计算y1,y2,y3的堆叠占比
+dataset['y1_stack'] = dataset['y1']
+dataset['y2_stack'] = [y1 + y2 for y1, y2 in zip(dataset['y1'], dataset['y2'])]
+dataset['y3_stack'] = [y1 + y2 + y3 for y1, y2, y3 in zip(dataset['y1'], dataset['y2'], dataset['y3'])]
+
+dataset['y1_text'] = ['%s(%s%%)' % (y1, y1 * 100 / y3_s) for y1, y3_s in zip(dataset['y1'], dataset['y3_stack'])]
+dataset['y2_text'] = ['%s(%s%%)' % (y2, y2 * 100 / y3_s) for y2, y3_s in zip(dataset['y2'], dataset['y3_stack'])]
+dataset['y3_text'] = ['%s(%s%%)' % (y3, y3 * 100 / y3_s) for y3, y3_s in zip(dataset['y3'], dataset['y3_stack'])]
+
+data_g = []
+tr_1 = Scatter(
+    x=dataset['x'],
+    y=dataset['y1_stack'],
+    text=dataset['y1_text'],
+    hoverinfo='x+text',
+    mode='lines',
+    name='Shanghai',
+    fill='tozeroy'  # 填充方式: 到x轴
+)
+data_g.append(tr_1)
+
+tr_2 = Scatter(
+    x=dataset['x'],
+    y=dataset['y2_stack'],
+    text=dataset['y2_text'],
+    hoverinfo='x+text',
+    mode='lines',
+    name='Beijing',
+    fill='tonexty'  # 填充方式:到下方的另一条线
+)
+data_g.append(tr_2)
+
+tr_3 = Scatter(
+    x=dataset['x'],
+    y=dataset['y3_stack'],
+    text=dataset['y3_text'],
+    hoverinfo='x+text',
+    mode='lines',
+    name='Chengdu',
+    fill='tonexty'
+)
+data_g.append(tr_3)
+
+layout = Layout(title="field area plots", xaxis={'title': 'x'}, yaxis={'title': 'value'})
+fig = Figure(data=data_g, layout=layout)
+py.plot(fig, filename="pie")
--- python.py
+++ python.py
@@ -74,14 +74,23 @@ for j in range(0,3,1):
             else:
                 comments = 'NEW'
 
+            if tag.find('span',class_='_1uyixqdu'):
+                scoreTmp = tag.find('span',class_='_1uyixqdu')
+                print(scoreTmp)
+                score = scoreTmp.find_all('span')[0].get('aria-label').replace('评分是', '').replace('（满分为5）', '')
+            else:
+                score = '0'
+
             print(url)
             print(title)
             print(price)
             print(type)
-            print(comments + '\n')
+            print(comments)
+            print(score)
+            print("\n")
 
-            sql = "INSERT INTO Airbnb_"+city+"(url,price,type,comments) values('" \
-                  + url +"'," + price + ",'" + type + "','" + comments +"')"
+            sql = "INSERT INTO Airbnb_" + city + "(url,price,type,comments,score) values('" \
+                  + url + "','" + price + "','" + type + "','" + comments + "','" + score + "')"
             print(sql)
             cursor.execute(sql)
             con.commit()
@@ -253,3 +262,115 @@ print(BeijingPrice)
 data = Data([Chengdu,Shanghai,Beijing])
 
 py.plot(data, filename = 'compare')
+
+city = ''
+y1 = ''
+y2 = ''
+y3 = ''
+#分数比较
+for i in range(0,3,1):
+    if i == 2:
+        city = 'Shanghai'
+    if i == 1:
+        city = 'Beijing'
+    if i == 0:
+        city = 'Chengdu'
+
+    sql = " SELECT (SELECT COUNT(*) FROM airbnb_"+city+" AS "+city+" WHERE "+city+".score = 3) AS '3' ," \
+				"IFNULL((SELECT AVG(price) FROM airbnb_"+city+" AS "+city+" WHERE "+city+".score = 3),0) AS '3Price' ," \
+				"(SELECT COUNT(*) FROM airbnb_"+city+" AS "+city+" WHERE "+city+".score = 4) AS '4' ," \
+				"IFNULL((SELECT AVG(price) FROM airbnb_"+city+" AS "+city+" WHERE "+city+".score = 4),0) AS '4Price' ," \
+				"(SELECT COUNT(*) FROM airbnb_"+city+" AS "+city+" WHERE "+city+".score = 4.5) AS '4.5' ," \
+				"IFNULL((SELECT AVG(price) FROM airbnb_"+city+" AS "+city+" WHERE "+city+".score = 4.5),0) AS '4.5Price' ," \
+				"(SELECT COUNT(*) FROM airbnb_"+city+" AS "+city+" WHERE "+city+".score = 5) AS '5' ," \
+				"IFNULL((SELECT AVG(price) FROM airbnb_"+city+" AS "+city+" WHERE "+city+".score = 5),0) AS '5Price' " \
+            "FROM airbnb_"+city+" LIMIT 1 "
+
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    print(result)
+
+    price=[int(result[0][1]),int(result[0][3]),int(result[0][5]),int(result[0][7])]
+    num=[int(result[0][0]),int(result[0][2]),int(result[0][4]),int(result[0][6])]
+
+    print(price)
+
+    if i == 2:
+        Shanghai = Scatter(
+            x=['3','4','4.5','5'],
+            y=price,
+            name='Shanghai'
+        )
+        y1 = num
+    if i == 1:
+        Beijing = Scatter(
+            x=['3', '4', '4.5', '5'],
+            y=price,
+            name='Beijing'
+        )
+        y2 = num
+    if i == 0:
+        Chengdu = Scatter(
+            x=['3', '4', '4.5', '5'],
+            y=price,
+            name='Chengdu'
+        )
+        y3 = num
+
+
+data = Data([Chengdu,Shanghai,Beijing])
+
+py.plot(data, filename = 'score')
+
+#各个评分段的房屋个数
+dataset = {'x': ['3', '4', '4.5', '5'],
+           'y1': y1,
+           'y2': y2,
+           'y3': y3}
+
+# 计算y1,y2,y3的堆叠占比
+dataset['y1_stack'] = dataset['y1']
+dataset['y2_stack'] = [y1 + y2 for y1, y2 in zip(dataset['y1'], dataset['y2'])]
+dataset['y3_stack'] = [y1 + y2 + y3 for y1, y2, y3 in zip(dataset['y1'], dataset['y2'], dataset['y3'])]
+
+dataset['y1_text'] = ['%s(%s%%)' % (y1, y1 * 100 / y3_s) for y1, y3_s in zip(dataset['y1'], dataset['y3_stack'])]
+dataset['y2_text'] = ['%s(%s%%)' % (y2, y2 * 100 / y3_s) for y2, y3_s in zip(dataset['y2'], dataset['y3_stack'])]
+dataset['y3_text'] = ['%s(%s%%)' % (y3, y3 * 100 / y3_s) for y3, y3_s in zip(dataset['y3'], dataset['y3_stack'])]
+
+data_g = []
+tr_1 = Scatter(
+    x=dataset['x'],
+    y=dataset['y1_stack'],
+    text=dataset['y1_text'],
+    hoverinfo='x+text',
+    mode='lines',
+    name='Shanghai',
+    fill='tozeroy'  # 填充方式: 到x轴
+)
+data_g.append(tr_1)
+
+tr_2 = Scatter(
+    x=dataset['x'],
+    y=dataset['y2_stack'],
+    text=dataset['y2_text'],
+    hoverinfo='x+text',
+    mode='lines',
+    name='Beijing',
+    fill='tonexty'  # 填充方式:到下方的另一条线
+)
+data_g.append(tr_2)
+
+tr_3 = Scatter(
+    x=dataset['x'],
+    y=dataset['y3_stack'],
+    text=dataset['y3_text'],
+    hoverinfo='x+text',
+    mode='lines',
+    name='Chengdu',
+    fill='tonexty'
+)
+data_g.append(tr_3)
+
+layout = Layout(title="field area plots", xaxis={'title': 'x'}, yaxis={'title': 'value'})
+fig = Figure(data=data_g, layout=layout)
+py.plot(fig, filename="pie")
